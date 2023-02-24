6dde5329897996112fa534c14c27661029f08233
Roman <122548495+RomanPerchynskyi@users.noreply.github.com>
2023-02-24 10:48:07 +0100

    Add files via upload

---
+++ Homework_FE_4/script/script.js
@@ -0,0 +1,36 @@
+
+
+
+const Red = document.querySelector('.red');
+
+Red.addEventListener('click', function(){
+    alert(" #ff0000 'Красный!'") ;
+})
+
+
+const Blue = document.querySelector('.blue');
+
+Blue.addEventListener('click', function(){
+    alert(" #0000ff 'Синий!'") ;
+})
+
+
+const Green = document.querySelector('.green');
+
+Green.addEventListener('click', function(){
+    alert(" #00ff00 'Зеленый!'") ;
+})
+
+
+const Yellow = document.querySelector('.yellow');
+
+Yellow.addEventListener('click', function(){
+    alert(" #ffff00 'Желтый!'") ;
+})
+
+
+const Purple = document.querySelector('.purple');
+
+Purple.addEventListener('click', function(){
+    alert(" #800080 'Фиолетовый!'") ;
+})
